1437e351014aa08e0f0ff17c069623a0d9ae07b6
marvian <marvianmontoya@hotmail.com>
2019-12-23 16:29:09 -0400

    reporte listo

---
+++ src/app/modelos/listaNegra.ts
@@ -0,0 +1,11 @@
+export class ListaNegra {
+
+   public id: 						number;
+   public fecha_creacion:			string;
+   public fecha_caducidad: 			string;
+   public porcentaje:	 			number;
+   public tipo:						string;
+   public descripcion: 				string;
+   public cantidad_mt_por_mo:		number; 
+ 
+ }
